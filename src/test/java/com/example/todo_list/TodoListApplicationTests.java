555a8ad3456d0d6ee19a4dedaf4b3436a5a34b89
Rahul Nayak <rahul_nayak@thbs.india.com>
2024-02-01 14:38:18 +0530

    Update files

--- src/test/java/com/example/todo_list/TodoListApplicationTests.java
+++ src/test/java/com/example/todo_list/TodoListApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.todo_list;
+// package com.example.todo_list;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TodoListApplicationTests {
+// @SpringBootTest
+// class TodoListApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }

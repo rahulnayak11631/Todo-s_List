10b5743e23ae19c913b3d27e296be7089f3dd005
rahul nayak <rahul_nayak@thbs.com>
2024-03-28 15:15:12 +0530

    update app.props

--- src/main/java/com/example/todo_list/controller/todo_controller.java
+++ src/main/java/com/example/todo_list/controller/todo_controller.java
@@ -3,6 +3,7 @@ package com.example.todo_list.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class todo_controller {
     @Autowired
     private todo_service todo_service_;
